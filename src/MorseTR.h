c37cf26cd3e66361c85ffad8e4a89e52007201de
pmozen <77512499+pmozen@users.noreply.github.com>
2024-06-09 00:14:03 +0300

    Add files via upload

---
+++ src/MorseTR.h
@@ -0,0 +1,21 @@
+/*
+  MorseTR.h - Library for Turkish flashing Morse code.
+  Created by özenSoftware, June 8, 2024.
+  Released into the public domain.
+*/
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+
+#endif
